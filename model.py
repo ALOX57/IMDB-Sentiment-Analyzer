454d783bbd67099d77d41034b4e52e7f0df56df6
Alexander Birtles <aloxemail@gmail.com>
2025-05-11 13:02:58 +1000

    Add TF_IDF vectorisation of movie reviews

--- model.py
+++ model.py
@@ -8,3 +8,10 @@ df = pd.read_csv("IMDB Dataset.csv")
 
 # Convert 'positive'/'negative' into 1 / 10
 df["label"] = df["sentiment"].map({"positive": 1, "negative": 0})
+
+x = df["review"]
+y = df["label"]
+
+# Convert text into TF-IDF vectors
+vectorizer = TfidfVectorizer(stop_words="english", max_features=5000)
+x_vec = vectorizer.fit_transform(x)
